5f17ff7288988a85ed744032c7d037bd92ffea6a
Lesther Josue Gonzalez Aguilar <57812506+LestherGonzalezAguilar@users.noreply.github.com>
2024-06-15 11:51:29 -0600

    Feat: LoginPage.js in index and script.js only another pages

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,4 @@
-function validateLogin() {
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  const errorMessage = document.getElementById('error-message');
 
-  if (password === 'password') {
-    window.location.href = 'menu.html';
-    sessionStorage.setItem("token", Date.now().toString())
-    sessionStorage.setItem("userName", `${username}`)
-    return false;
-  } else {
-    errorMessage.textContent = 'Usuario o contraseña incorrectos.';
-    return false;
-  }
-}
 
 function toggleSidebar() {
   const sidebar = document.querySelector('#sideBar');
@@ -28,10 +14,7 @@ function logout(){
   sessionStorage.clear()
   location.href = '/'
 }
-// function toggleSidebar() {
-//   const sidebar = document.getElementById('sidebar');
-//   sidebar.style.left = sidebar.style.left === '0px' ? '-200px' : '0px';
-// }
+
 
 function verGrafico() {
   // Lógica para generar el gráfico
